ab956cb458c4868cf7fd6602d77706b50c45e850
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-09-24 18:32:31 -0300

    Arrumando diretórios css e js

---
+++ js/infowindow_template_estadual.js
@@ -0,0 +1,8 @@
+<div class="cartodb-tooltip-content-wrapper">
+  <div class="cartodb-tooltip-content">
+    <h5>{{nome_ibge_com_acento}} ({{estado}}) - {{cargo}}</h5>
+    <h6>{{num_urna_cand}} ({{partido}}) - {{valor_perc}}%</h6>
+    <p>{{valor_abs}} votos</p>
+    <p>Eleitorado do município:<br/> {{eleitorado}} eleitores</p>
+  </div>
+</div>
