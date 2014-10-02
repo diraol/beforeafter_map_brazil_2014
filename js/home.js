6856ba3cb723b34536c6c3c6a786fbd31a571b4a
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-10-02 19:07:26 -0300

    Modificações para versão de teste, usando base fake do tse '2015'

--- js/home.js
+++ js/home.js
@@ -22,54 +22,54 @@ function _generate_map(container, ano, cargo, uf, nurna){
           ORDER BY\
             valor_perc,\
             cartodb_id",
-    cartocss: "#resultado_2014 {\
+    cartocss: "#resultado_2015 {\
                     polygon-opacity: 0.5;\
                     line-color: #444;\
                     line-width: 0.1;\
                     line-opacity: 0.3; }\
-                #resultado_2014[partido='PBAN'] { polygon-fill: #A6CEE3; }\
-                #resultado_2014[partido='PBAR'] { polygon-fill: #1F78B4; }\
-                #resultado_2014[partido='PLAN'] { polygon-fill: #B2DF8A; }\
-                #resultado_2014[partido='PGAL'] { polygon-fill: #33A02C; }\
-                #resultado_2014[partido='PJAG'] { polygon-fill: #FB9A99; }\
-                #resultado_2014[partido='PARA'] { polygon-fill: #E31A1C; }\
-                #resultado_2014[partido='PKUW'] { polygon-fill: #FDBF6F; }\
-                #resultado_2014[partido='PT'] { polygon-fill: " + cores_partidos['PT']  + "; }\
-                #resultado_2014[partido='PSTU'] { polygon-fill: " + cores_partidos['PSTU']  + "; }\
-                #resultado_2014[partido='PCDOB'] { polygon-fill: " + cores_partidos['PCDOB']  + "; }\
-                #resultado_2014[partido='PCB'] { polygon-fill: " + cores_partidos['PCB']  + "; }\
-                #resultado_2014[partido='PSL'] { polygon-fill: " + cores_partidos['PSL']  + "; }\
-                #resultado_2014[partido='PCO'] { polygon-fill: " + cores_partidos['PCO']  + "; }\
-                #resultado_2014[partido='PSOL'] { polygon-fill: " + cores_partidos['PSOL']  + "; }\
-                #resultado_2014[partido='PMDB'] { polygon-fill: " + cores_partidos['PMDB']  + "; }\
-                #resultado_2014[partido='PROS'] { polygon-fill: " + cores_partidos['PROS']  + "; }\
-                #resultado_2014[partido='PPL'] { polygon-fill: " + cores_partidos['PPL']  + "; }\
-                #resultado_2014[partido='PSB'] { polygon-fill: " + cores_partidos['PSB']  + "; }\
-                #resultado_2014[partido='PTB'] { polygon-fill: " + cores_partidos['PTB']  + "; }\
-                #resultado_2014[partido='PDT'] { polygon-fill: " + cores_partidos['PDT']  + "; }\
-                #resultado_2014[partido='PPS'] { polygon-fill: " + cores_partidos['PPS']  + "; }\
-                #resultado_2014[partido='SD'] { polygon-fill: " + cores_partidos['SD']  + "; }\
-                #resultado_2014[partido='PSC'] { polygon-fill: " + cores_partidos['PSC']  + "; }\
-                #resultado_2014[partido='PTDOB'] { polygon-fill: " + cores_partidos['PTDOB']  + "; }\
-                #resultado_2014[partido='PMN'] { polygon-fill: " + cores_partidos['PMN']  + "; }\
-                #resultado_2014[partido='PSD'] { polygon-fill: " + cores_partidos['PSD']  + "; }\
-                #resultado_2014[partido='PEN'] { polygon-fill: " + cores_partidos['PEN']  + "; }\
-                #resultado_2014[partido='PR'] { polygon-fill: " + cores_partidos['PR']  + "; }\
-                #resultado_2014[partido='PV'] { polygon-fill: " + cores_partidos['PV']  + "; }\
-                #resultado_2014[partido='PP'] { polygon-fill: " + cores_partidos['PP']  + "; }\
-                #resultado_2014[partido='PTC'] { polygon-fill: " + cores_partidos['PTC']  + "; }\
-                #resultado_2014[partido='PTN'] { polygon-fill: " + cores_partidos['PTN']  + "; }\
-                #resultado_2014[partido='PRB'] { polygon-fill: " + cores_partidos['PRB']  + "; }\
-                #resultado_2014[partido='PSDC'] { polygon-fill: " + cores_partidos['PSDC']  + "; }\
-                #resultado_2014[partido='PRTB'] { polygon-fill: " + cores_partidos['PRTB']  + "; }\
-                #resultado_2014[partido='PHS'] { polygon-fill: " + cores_partidos['PHS']  + "; }\
-                #resultado_2014[partido='PRP'] { polygon-fill: " + cores_partidos['PRP']  + "; }\
-                #resultado_2014[partido='DEM'] { polygon-fill: " + cores_partidos['DEM']  + "; }\
-                #resultado_2014[partido='PSDB'] { polygon-fill: " + cores_partidos['PSDB']  + "; }",
+                #resultado_2015[partido='PBAN'] { polygon-fill: #A6CEE3; }\
+                #resultado_2015[partido='PBAR'] { polygon-fill: #1F78B4; }\
+                #resultado_2015[partido='PLAN'] { polygon-fill: #B2DF8A; }\
+                #resultado_2015[partido='PGAL'] { polygon-fill: #33A02C; }\
+                #resultado_2015[partido='PJAG'] { polygon-fill: #FB9A99; }\
+                #resultado_2015[partido='PARA'] { polygon-fill: #E31A1C; }\
+                #resultado_2015[partido='PKUW'] { polygon-fill: #FDBF6F; }\
+                #resultado_2015[partido='PT'] { polygon-fill: " + cores_partidos['PT']  + "; }\
+                #resultado_2015[partido='PSTU'] { polygon-fill: " + cores_partidos['PSTU']  + "; }\
+                #resultado_2015[partido='PCDOB'] { polygon-fill: " + cores_partidos['PCDOB']  + "; }\
+                #resultado_2015[partido='PCB'] { polygon-fill: " + cores_partidos['PCB']  + "; }\
+                #resultado_2015[partido='PSL'] { polygon-fill: " + cores_partidos['PSL']  + "; }\
+                #resultado_2015[partido='PCO'] { polygon-fill: " + cores_partidos['PCO']  + "; }\
+                #resultado_2015[partido='PSOL'] { polygon-fill: " + cores_partidos['PSOL']  + "; }\
+                #resultado_2015[partido='PMDB'] { polygon-fill: " + cores_partidos['PMDB']  + "; }\
+                #resultado_2015[partido='PROS'] { polygon-fill: " + cores_partidos['PROS']  + "; }\
+                #resultado_2015[partido='PPL'] { polygon-fill: " + cores_partidos['PPL']  + "; }\
+                #resultado_2015[partido='PSB'] { polygon-fill: " + cores_partidos['PSB']  + "; }\
+                #resultado_2015[partido='PTB'] { polygon-fill: " + cores_partidos['PTB']  + "; }\
+                #resultado_2015[partido='PDT'] { polygon-fill: " + cores_partidos['PDT']  + "; }\
+                #resultado_2015[partido='PPS'] { polygon-fill: " + cores_partidos['PPS']  + "; }\
+                #resultado_2015[partido='SD'] { polygon-fill: " + cores_partidos['SD']  + "; }\
+                #resultado_2015[partido='PSC'] { polygon-fill: " + cores_partidos['PSC']  + "; }\
+                #resultado_2015[partido='PTDOB'] { polygon-fill: " + cores_partidos['PTDOB']  + "; }\
+                #resultado_2015[partido='PMN'] { polygon-fill: " + cores_partidos['PMN']  + "; }\
+                #resultado_2015[partido='PSD'] { polygon-fill: " + cores_partidos['PSD']  + "; }\
+                #resultado_2015[partido='PEN'] { polygon-fill: " + cores_partidos['PEN']  + "; }\
+                #resultado_2015[partido='PR'] { polygon-fill: " + cores_partidos['PR']  + "; }\
+                #resultado_2015[partido='PV'] { polygon-fill: " + cores_partidos['PV']  + "; }\
+                #resultado_2015[partido='PP'] { polygon-fill: " + cores_partidos['PP']  + "; }\
+                #resultado_2015[partido='PTC'] { polygon-fill: " + cores_partidos['PTC']  + "; }\
+                #resultado_2015[partido='PTN'] { polygon-fill: " + cores_partidos['PTN']  + "; }\
+                #resultado_2015[partido='PRB'] { polygon-fill: " + cores_partidos['PRB']  + "; }\
+                #resultado_2015[partido='PSDC'] { polygon-fill: " + cores_partidos['PSDC']  + "; }\
+                #resultado_2015[partido='PRTB'] { polygon-fill: " + cores_partidos['PRTB']  + "; }\
+                #resultado_2015[partido='PHS'] { polygon-fill: " + cores_partidos['PHS']  + "; }\
+                #resultado_2015[partido='PRP'] { polygon-fill: " + cores_partidos['PRP']  + "; }\
+                #resultado_2015[partido='DEM'] { polygon-fill: " + cores_partidos['DEM']  + "; }\
+                #resultado_2015[partido='PSDB'] { polygon-fill: " + cores_partidos['PSDB']  + "; }",
   };
 
   var options = {
-        title: "Eleições 2014 - Apuração",
+        title: "Eleições 2015 - Apuração",
         shareable: false,
         searchControl: false,
         layer_selector: false,
@@ -130,5 +130,5 @@ function _generate_map(container, ano, cargo, uf, nurna){
 }
 
 function main(){
-    var mapa = _generate_map("mapa", "2014", "presidente", "br", "");
+    var mapa = _generate_map("mapa", "2015", "presidente", "br", "");
 }
