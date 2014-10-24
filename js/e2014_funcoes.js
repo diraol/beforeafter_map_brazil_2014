76c80e1da387a7499183f906807707add5540caf
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-10-24 19:19:20 -0200

    Fixing bugs

--- js/e2014_funcoes.js
+++ js/e2014_funcoes.js
@@ -245,7 +245,5 @@ function _build_tooltip_query(year, round, cargo, uf_viz, uf, cod_tse_municipio)
 function toggleSearchField(){
     $(".cartodb-searchbox form input").toggleClass('hidden');
     $(".cartodb-searchbox form button").toggle();
-    //$(".cartodb-searchbox button.submit").toggle();
-    //$(".cartodb-searchbox button.reduce").toggle();
-    //$(el).toggle();
 }
+
